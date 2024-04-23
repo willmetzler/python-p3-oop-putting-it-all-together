673ba27b589e7760b2b1ed273587b628de1e7f19
Will Metzler <metzlerwill@gmail.com>
2024-04-23 14:29:23 -0400

    finish lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
+
+    def __init__(self,title:str, page_count):
+        self.title = title
+        self._page_count = page_count
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self,value):
+        if value != int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+        
+
     pass
     
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self,brand:str, size):
+        self.brand = brand
+        self._size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print ("Your shoe is as good as new!")
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,value):
+        if value != int:
+            print("size must be an integer")
+        else:
+            self._size = value
+
+pass
